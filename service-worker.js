c11e7a461611b21f8482ceaa7c7131161dfa265f
MY8010-application <ch.mp.masa@gmail.com>
2026-08-10 14:13:34 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,25 @@
+const CACHE_NAME = "tsutore-v1";
 
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        fetch(event.request).catch(() =>
+            caches.match(event.request)
+        )
+    );
+});
